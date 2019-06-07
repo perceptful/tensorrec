e769310912e7233ba8f99cac658539148dc3919e
mohsaadi <msaadi@MSAADI-XPS.Medrah.jo>
2019-06-07 19:54:44 +0300

    Added Export func

--- tensorrec/tensorrec.py
+++ tensorrec/tensorrec.py
@@ -915,3 +915,28 @@ class TensorRec(object):
             model = pickle.load(file=file)
         model._attach_graph_hooks()
         return model
+    
+    def export(directory_path, model_version):
+        """
+        Exports model and saves it to directory_path for Tensorflow serving.
+        :param directory_path: str
+        The path to the directory containing the saved model.
+        :return:
+        """
+        export_path_base = directory_path
+        export_path = os.path.join(
+            compat.as_bytes(export_path_base),
+            compat.as_bytes(str(FLAGS.model_version)))
+        print 'Exporting trained model to', export_path
+        builder = tf.saved_model.builder.SavedModelBuilder(export_path)
+        builder.add_meta_graph_and_variables(
+            sess, [tf.saved_model.tag_constants.SERVING],
+            signature_def_map={
+                'predict_images':
+                    prediction_signature,
+                signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
+                    classification_signature,
+            },
+            main_op=tf.tables_initializer())
+        builder.save()
+
