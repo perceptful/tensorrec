b5298af393f09e4371a0815a5207c44c252b6ee5
msaadi <mohsaadi@hotmail.com>
2019-06-09 05:01:36 +0300

    And more fixes

--- tensorrec/tensorrec.py
+++ tensorrec/tensorrec.py
@@ -925,18 +925,12 @@ class TensorRec(object):
         """
         export_path_base = directory_path
         export_path = os.path.join(
-            compat.as_bytes(export_path_base),
-            compat.as_bytes(str(FLAGS.model_version)))
+            tf.compat.as_bytes(export_path_base),
+            tf.compat.as_bytes(str(tf.app.flags.FLAGS.model_version)))
         print('Exporting trained model to'), export_path
-        builder = tf.saved_model.builder.SavedModelBuilder(export_path)
-        builder.add_meta_graph_and_variables(
-            sess, [tf.saved_model.tag_constants.SERVING],
-            signature_def_map={
-                'predict_images':
-                    prediction_signature,
-                signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
-                    classification_signature,
-            },
-            main_op=tf.tables_initializer())
-        builder.save()
+        tf.saved_model.simple_save(get_session,
+            export_path,
+            inputs={"x": self.tf_n_sampled_items},
+            outputs={"z": self.tf_similar_items_ids})
 
+        print('Done exporting!')
